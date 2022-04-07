a14166ca0ae8f13628ed09d4f74911e39c809041
Neil Lobo <neil_edward_lobo@hotmail.com>
2022-04-07 00:55:34 -0400

    replace sqlite for mariadb; created api key validation middleware

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const MAP = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 const express = require("express");
 const multer  = require('multer')
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('../sql/cdn.db')
 const bodyParser = require('body-parser');
+const uuid = require("uuid");
+const mariadb = require('mariadb');
 require("dotenv").config();
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "IMG_UPLOADER",
+    connectionLimit: 5
+})
+
+// pool.getConnection()
+// .then(async (conn) => {
+//     const rows = await conn.query("SELECT * FROM API_KEYS")
+//     console.log(rows[0])
+// })
 
 const app = express();
 const opts = {
@@ -32,6 +45,34 @@ const opts = {
 }
 const upload = multer(opts).single("file")
 
+async function queryDB(query) {
+    let conn;
+    let rows;
+    try {
+        conn = await pool.getConnection();
+        rows = await conn.query(query);
+    } finally {
+        if (conn) {
+            conn.release();
+        }
+    }
+    return rows;
+}
+
+async function validKey(req, res, next) {
+    if (!req.headers.authorization) {
+        res.status(401).json({error: true, message: "No Authorization header found!"});
+        return
+    }   
+    const key = req.headers.authorization.split(" ")[1]
+    const rows = await queryDB(`SELECT * FROM API_KEYS WHERE \`KEY\` = '${key}'`)
+    if (rows[0]) {
+        next();
+    } else {
+        res.status(403).json({error: true, message: "Invalid API key"})
+    }
+}
+
 app.use(express.static("imgs"));
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -39,7 +80,7 @@ app.get("/", (req, res) => {
     res.sendFile("index.html", { root: __dirname })
 })
 
-app.post("/upload", (req,res) => {
+app.post("/upload", validKey, (req,res) => {
     upload(req, res, err => {
         if (err) {
             console.log(err)
@@ -51,17 +92,24 @@ app.post("/upload", (req,res) => {
             return;
         }
         
+        const fileId = req.file.filename.replace(".png", "");
         console.log("POST");
         const data = {
             error: null,
             message: "Image uploaded!",
-            fileName: req.file.filename
+            id: fileId
         }
         res.status(200).json(data);
     })
-
+    
 })
-// exports.app = app;
+
+app.get("/delete/:id", (req,res) => {
+    console.log(req.headers);
+    console.log(req.params)
+    console.log(req.query)
+    res.status(200).send();
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port: ${process.env.PORT}`)
